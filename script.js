6700c46f53b280bf1c27fd241c1db9ae4eb2d021
Nico Cabrera <152993723+NicolasCabrera7@users.noreply.github.com>
2023-12-13 18:57:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let intentos = 6;
+let diccionario = ['MANZANA', 'PERA', 'UVA', 'AGUACATE', 'SANDIA', 'BANANA']
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+const GRID = document.getElementById("grid");
+const ROW = document.createElement('div');
+ROW.className = 'row';
+
+
+
+function intentar(){
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra ) {
+        terminar("<h1>GANASTE!😀</h1>")
+        return
+    }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if(INTENTO[i] == undefined){
+            SPAN.innerHTML = '-';
+            SPAN.style.backgroundColor = 'grey';
+        }else if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        }else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        }else {//GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+    intentos--
+    if (intentos==0){
+        terminar("<h1>PERDISTE!😖</h1>")
+    }
+
+}
+
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input").value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}
+
+
+
+
